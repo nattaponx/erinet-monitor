4be88b2f5b369e7080980aa47845856fa23a9682
unknown <elarvic@SE00113722.ericsson.se>
2015-07-06 15:22:44 +0200

    Implemented d3, bootstrap, jquery and requirejs as node_modules and created the basic folder/file structure

---
+++ public/js/pages/dashboard.js
@@ -0,0 +1,6 @@
+require(['public/js/widgets/networkTopology.js'],function (NetworkTopology) {
+	NetworkTopology.init('dashboard-container-1-1', 'primary', 'box-1-1');
+	NetworkTopology.init('dashboard-container-1-2', 'warning', 'box-1-1');
+	NetworkTopology.init('dashboard-container-2-1', 'success', 'box-1-1');
+	NetworkTopology.init('dashboard-container-2-2', 'danger', 'box-1-1');
+});
