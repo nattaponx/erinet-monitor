c90221ade609559e52a7c2e6d1152a4b4ccc5f47
unknown <eronsop@SE00202241.ericsson.se>
2015-07-06 15:28:40 +0200

    addded dependencies

--- api/connect.js
+++ api/connect.js
@@ -1,37 +1,78 @@
-exports.connector = function(queryString) {
-    var sql = require('mssql'); 
- 
-    var config = {
-        user: 'web_guest',
-        password: 'web_guest',
-        server: 'selnpcndb01.seln.ete.ericsson.se', 
-        database: 'erinetdb',
-        
-        options: {
-            encrypt: false 
-        }
-    }
- 
-    var connection = new sql.Connection(config, function(err) {
-        // ... error checks 
-        
-        // Query 
-        
-        var request = new sql.Request(connection); // or: var request = connection.request(); 
-        request.query(queryString, function(err, recordset) {
-            // ... error checks 
-            console.log(err);
-            
-            console.dir(recordset);
-            var jsonData = JSON.stringify(recordset);
-            console.log(jsonData);
-            // recordset.forEach(function(row) {
-            //      console.log(row.NodeName + " " + row.StopDate);
-            // });
-        });
-        
-    });
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
 }
+
+define(["mssql"], function(mssql){
+  return {
+    fetchFromQuery: function(queryString, callback) {
+ 
+        var config = {
+            user: 'web_guest',
+            password: 'web_guest',
+            server: 'selnpcndb01.seln.ete.ericsson.se', 
+            database: 'erinetdb',            
+            options: {
+                encrypt: false 
+            }
+        }
+ 
+        var connection = new mssql.Connection(config, function(err) {
+            // ... error checks 
+            
+            // Query 
+            
+            var request = new mssql.Request(connection); // or: var request = connection.request(); 
+            request.query(queryString, function(err, recordset) {
+                // ... error checks 
+                // console.log(err);
+                
+                // console.dir(recordset);
+                var jsonData = JSON.stringify(recordset);
+                // recordset.forEach(function(row) {
+                //      console.log(row.NodeName + " " + row.StopDate);
+                // });
+                callback(jsonData);
+            });
+            
+        });
+    }
+  }
+});
+
+// exports.connector = function(queryString) {
+//     var sql = require('mssql'); 
+ 
+//     var config = {
+//         user: 'web_guest',
+//         password: 'web_guest',
+//         server: 'selnpcndb01.seln.ete.ericsson.se', 
+//         database: 'erinetdb',
+        
+//         options: {
+//             encrypt: false 
+//         }
+//     }
+ 
+//     var connection = new sql.Connection(config, function(err) {
+//         // ... error checks 
+        
+//         // Query 
+        
+//         var request = new sql.Request(connection); // or: var request = connection.request(); 
+//         request.query(queryString, function(err, recordset) {
+//             // ... error checks 
+//             console.log(err);
+            
+//             console.dir(recordset);
+//             var jsonData = JSON.stringify(recordset);
+//             console.log(jsonData);
+//             // recordset.forEach(function(row) {
+//             //      console.log(row.NodeName + " " + row.StopDate);
+//             // });
+//         });
+        
+//     });
+// }
 
 
 //var test1 = new Connector('select top 10 [Id],[Time],[Uplink traffic],[Downlink traffic] from dbo._EriNetGgsnPdcPayloadPerSec');
--- models/dummy.js
+++ models/dummy.js
@@ -1,4 +1,18 @@
-var connect = require('../api/connect'); 
-var result = connect.connector('select top 10 [Id],[Time],[Uplink traffic],[Downlink traffic]' + 
-								'from dbo._EriNetGgsnPdcPayloadPerSec');
-console.log(result);
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs(["../api/connect"], function(connect) {
+
+	connect.fetchFromQuery('select top 10 [Id],[Time],[Uplink traffic],[Downlink traffic]' + 
+										'from dbo._EriNetGgsnPdcPayloadPerSec', function(data){
+
+		console.log(data);
+	});
+
+});
