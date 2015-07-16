4e72e83abf1234052bc97693f54d597a47ac30d9
unknown <elarvic@SE00113722.ericsson.se>
2015-07-16 11:10:34 +0200

    Implemented basic 2x2 grid.

--- public/js/pages/dashboard.js
+++ public/js/pages/dashboard.js
@@ -11,11 +11,45 @@ require.config({
     },
  });
 
-require(['topology/networkTopologyController'],function (ntc) {
-	
+require([ 'node_modules/d3/d3.js','topology/networkTopologyController'], function (d3, ntc) {
+	 
 	//ntc.init('dashboard-container-1-1', 'primary', 'box-1-1');
 	//ntc.init('dashboard-container-1-2', 'warning', 'box-1-1');
 	//ntc.init('dashboard-container-2-1', 'success', 'box-1-1');
 	//ntc.init('dashboard-container-2-2', 'danger', 'box-1-1');
+	
+	resize();	
+	initContainers();
+
+	function initContainers () {
+		
+		d3.selectAll('.dashboard-container').append('div')
+			.attr('class', 'addBtn')
+			.on('mouseover', function(){
+				console.log('addBtn hover');
+			});
+
+		/*
+		for(var i = 1; i <= 2; i++){
+			for(var j = 1; j <= 2; j++){
+
+				d3.sel
+			}
+		}
+		*/
+	}
+
+	function resize(){
+		console.log('resize');
+
+		var neg 				  = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
+      	var window_height 	      = $(window).height();
+      	var sidebar_height 		  = $(".sidebar").height();
+      	var content_header_height = $('.content-header').outerHeight();
+      	var diff = window_height - neg - content_header_height;
+
+  		$(".content").css('min-height', diff);
+  		$(".dashboard-content-container").css('height', diff - 50);
+	}
 	
 });
