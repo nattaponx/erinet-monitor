58980627fce313376b6415277307c9f8530f4fe4
unknown <elarvic@SE00113722.ericsson.se>
2015-07-16 12:50:32 +0200

    Applied new background color to the containers and a new hover color on the addBtn

--- public/js/pages/dashboard.js
+++ public/js/pages/dashboard.js
@@ -65,7 +65,7 @@ require([ 'node_modules/d3/d3.js','topology/networkTopologyController'], functio
 				.attr('class', 'addBtn')
 				.attr('id', 'addBtn-' + id)
 				.on('mouseover', function(){
-					$('#addBtn-' + id).css('background-color', '#00285F')
+					$('#addBtn-' + id).css('background-color', '#3c8dbc')
 				})
 				.on('mouseout', function(){
 					$('#addBtn-' + id).css('background-color', 'white')
